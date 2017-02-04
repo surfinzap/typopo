b3adc45ffd14136ac4f759334dc4f9d0a6366872
Brano <surfin.zap@gmail.com>
2017-02-04 23:27:54 +0100

    create module — symbol replacements

---
+++ src/test/symbol-replacements.test.js
@@ -0,0 +1,23 @@
+import {replaceSymbols} from "../lib/symbol-replacements";
+import assert from 'assert';
+
+describe('Replace following symbols\n', () => {
+	let testCase = {
+		"(C) (C)": "© ©",
+		"(c) (c)": "© ©",
+		"(P) (P)": "Ⓟ Ⓟ",
+		"(p) (p)": "Ⓟ Ⓟ",
+		"(R) (R)": "® ®",
+		"(r) (r)": "® ®",
+		"(tm) (tm)": "™ ™",
+		"(TM) (TM)": "™ ™",
+		"+- +-": "± ±",
+		"-+ -+": "± ±",
+	};
+
+	Object.keys(testCase).forEach((key) => {
+		it("should replace following symbols", () => {
+			assert.equal(replaceSymbols(key), testCase[key]);
+		});
+	});
+});
