c629fc53b26763eed19caa1c313d610c8744f969
Brano <surfin.zap@gmail.com>
2019-01-12 14:22:19 +0100

    Create de-de.js locale

---
+++ src/locale/de-de.js
@@ -0,0 +1,20 @@
+const quotes = {
+	leftDoubleQuote : 				"„",
+	rightDoubleQuote : 				"“",
+	leftSingleQuote : 				"‚",
+	rightSingleQuote : 				"‘",
+};
+
+const numbers = {
+	ordinalIndicator :				"\\.",
+	romanOrdinalIndicator :		"\\.",
+}
+
+const abbreviationsForNbsp = []
+
+
+export default {
+	quotes,
+	numbers,
+	abbreviationsForNbsp
+}
