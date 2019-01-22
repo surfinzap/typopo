e6526f056515c94b107de988b29de1b36e488931
skurzinz <37441365+skurzinz@users.noreply.github.com>
2019-01-22 20:21:23 +0100

    Comment in lines.test.js

--- src/test/whitespace/lines.test.js
+++ src/test/whitespace/lines.test.js
@@ -4,6 +4,8 @@ import assert from 'assert';
 describe('Remove empty lines\n', () => {
 	let testCase = {
 		// Remove excessive empty lines between paragraphs
+		// Comment: Thinking a bit in LaTeX/markdown conventions, for me it would make sense to normalize two or more to two \n{2,} to \n\n. 
+		// Moreover, how does that \n test deal with the different newlines (\r|\l)? 
 		"something here\nand over there\n\nand over there\n\n\nand over there":
 		"something here\nand over there\nand over there\nand over there",
 	};
