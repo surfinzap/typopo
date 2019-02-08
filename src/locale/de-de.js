270373d21430b5392910424f4ef4bb5a566d73c3
Brano <surfin.zap@gmail.com>
2019-02-08 15:25:29 +0100

    Include German abbreviations for later work

--- src/locale/de-de.js
+++ src/locale/de-de.js
@@ -21,3 +21,26 @@ export default {
 	numbers,
 	abbreviationsForNbsp
 }
+
+
+// b. w.
+// d. h.
+// d. i.
+// n. Chr.
+// s. a.
+// s. o.
+// s. u.
+// u. a.
+// u. ä.
+// u. Ä.
+// u. U.
+// u. z.
+// u. zw.
+// v. a.
+// v. Chr.
+// z. B.
+// z. T.
+// z. Zt.
+// e. V.
+// v. u. Z.
+// n. u. Z.
