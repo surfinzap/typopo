7dc2d57f066764a4c541b23033762357a58b08a2
Brano <surfin.zap@gmail.com>
2019-10-02 11:22:36 +0200

    Bump version → 2.2.4

--- src/typopo.js
+++ src/typopo.js
@@ -1,10 +1,10 @@
 /*!
- * Typopo 2.2.3
+ * Typopo 2.2.4
  *
  * Copyright 2015-19 Braňo Šandala
  * Released under the MIT license
  *
- * Date: 2019-10-01
+ * Date: 2019-10-02
  */
 
 import Locale from "./locale/locale";
