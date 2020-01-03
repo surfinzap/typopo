408390168920d372ee21f8e6deb69da0533458f3
Brano <surfin.zap@gmail.com>
2020-01-03 11:56:05 +0100

    Update build system and dependencies

--- gulpfile-old.js
+++ gulpfile-old.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
-
 const browserify = require('browserify');
+
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const browsersync = require("browser-sync").create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
 const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 
 var paths = {
+	dev: {
+		html: 'demo/typopo-demo.html',
+		index: 'typopo-demo.html',
+		name: 'typopo_browser.built.js',
+		dest: 'build/'
+	},
 	browser: {
 		src: 'src/browser_typopo.js',
 		name: 'typopo.min.js',
@@ -24,39 +32,77 @@ var paths = {
  * Define our tasks using plain functions
  */
 
+function devBrowserBuild() {
+	 return browserify({entries: paths.browser.src, debug: true})
+		 .transform("babelify")
+		 .bundle()
+		 .pipe(source(paths.dev.name))
+		 .pipe(buffer())
+		 .pipe(sourcemaps.init())
+		 .pipe(sourcemaps.write('./maps'))
+		 .pipe(gulp.dest(paths.dev.dest))
+ };
+
+
 function browserBuild() {
 	return browserify({ entries: paths.browser.src, debug: false })
-		.transform(babelify)
-		.bundle()
-		.pipe(source(paths.browser.name))
-		.pipe(buffer())
-		.pipe(uglify())
-		.pipe(gulp.dest(paths.browser.dest));
+	.transform(babelify)
+	.bundle()
+	.pipe(source(paths.browser.name))
+	.pipe(buffer())
+	.pipe(uglify())
+	.pipe(gulp.dest(paths.browser.dest));
 }
 
 function npmBuild() {
-	return gulp.src(paths.npm.src, { sourcemaps: false })
-		.pipe(babel())
+	return browserify({entries: paths.npm.src}, {standalone: 'typopo'})
+		.transform("babelify")
+		.bundle()
+		.pipe(source(paths.npm.name))
+		.pipe(buffer())
 		.pipe(uglify())
-		.pipe(concat(paths.npm.name))
-		.pipe(gulp.dest(paths.npm.dest));
+		.pipe(gulp.dest(paths.npm.dest))
+};
+
+function copyHtmlToDest() {
+	return gulp
+		.src(paths.dev.html)
+		.pipe(gulp.dest(paths.dev.dest));
+};
+
+function watchFiles() {
+	gulp.watch(
+		[
+			paths.npm.src,
+			paths.browser.src,
+			paths.dev.html,
+			"./src/**/*",
+		],
+			gulp.series(devBrowserBuild, copyHtmlToDest, browserSyncReload)
+		);
 }
 
-function watch() {
-	gulp.watch(paths.npm.src, npmBuild);
-	gulp.watch(paths.browser.src, browserBuild);
+// BrowserSync
+function browserSync(done) {
+	browsersync.init({
+		server: {
+			baseDir: 'build/',
+			index: 'typopo-demo.html'
+		},
+		port: 3000
+	});
+	done();
 }
 
-/*
- * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
- */
-var build = gulp.parallel(npmBuild, browserBuild);
+// BrowserSync Reload
+function browserSyncReload(done) {
+	browsersync.reload();
+	done();
+}
 
-/*
- * You can use CommonJS `exports` module notation to declare tasks
- */
+const watch = gulp.parallel(watchFiles, browserSync);
+const build = gulp.parallel(npmBuild, browserBuild);
 
-exports.scripts = npmBuild;
 exports.watch = watch;
 exports.build = build;
 /*
