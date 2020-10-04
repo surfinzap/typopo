15987450cde079bd2e329001e0911552c5780ba4
Matúš Duchoň <matus.duchon@gmail.com>
2020-10-04 12:06:37 +0200

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "root": true,
+  "env": {
+    "node": true,
+    "browser": true,
+    "es6": true,
+    "mocha": true
+  },
+  "extends": [
+    "eslint:recommended"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "sourceType": 'module',
+    "parser": "babel-eslint"
+  },
+  "rules": {}
+}
