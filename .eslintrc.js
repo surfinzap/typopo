2b22b9738925144207b2980bbbaf71ea52f2c1c2
Matúš Duchoň <matus.duchon@gmail.com>
2020-10-04 12:42:10 +0200

    Set up prettier
    :lipstick:

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-  "root": true,
-  "env": {
-    "node": true,
-    "browser": true,
-    "es6": true,
-    "mocha": true
-  },
-  "extends": [
-    "eslint:recommended"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 2020,
-    "sourceType": 'module',
-    "parser": "babel-eslint"
-  },
-  "rules": {}
+	root: true,
+	env: {
+		node: true,
+		browser: true,
+		es6: true,
+		mocha: true,
+	},
+	extends: ['eslint:recommended', 'plugin:prettier/recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		parser: 'babel-eslint',
+	},
+	plugins: ['prettier'],
+	rules: {},
 }
