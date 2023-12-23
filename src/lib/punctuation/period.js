4c44e4d6d324f731dd544e00656f1e648437de42
Brano Sandala <surfinzap@users.noreply.github.com>
2023-12-23 20:51:36 +0100

    🐛 Fix char escaping

--- src/lib/punctuation/period.js
+++ src/lib/punctuation/period.js
@@ -1,7 +1,14 @@
 /* replace 2 periods at the end of the sentence with a single period */
 export function removeExtraPeriod(string) {
-	return string.replace(/\.{2}(?![
-		/\\])/g, ".");
+
+
+	return string.replace(
+		new RegExp(
+			"\\.{2}(?![\\\\/])",
+			"g"
+		),
+		"."
+	);
 }
 
 export function fixPeriod(string) {
