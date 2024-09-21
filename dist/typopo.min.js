ccabf0299f0bf38366d2b0991aef93932f1320b3
Brano Sandala <surfinzap@users.noreply.github.com>
2024-09-21 19:27:11 +0200

    Test update

--- dist/typopo.min.js
+++ dist/typopo.min.js
@@ -1,5 +1,5 @@
 /*!
- * Typopo v2.5.7 (https://typopo.org)
+ * Typopo v2.5.8 (https://typopo.org)
  * Copyright 2015–2024 Braňo Šandala (https://brano.me)
  * Licensed under MIT (https://github.com/surfinzap/typopo/blob/main/LICENSE.txt)
  */
