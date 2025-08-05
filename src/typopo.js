7fc2f20a422727ce852b3c7f1a9ecbc9a6d5bd27
brano <hi@brano.me>
2025-08-05 21:04:08 +0200

    Format: typopo

--- src/typopo.js
+++ src/typopo.js
@@ -4,28 +4,26 @@
  * Licensed under MIT (https://github.com/surfinzap/typopo/blob/main/LICENSE.txt)
  */
 import Locale from "./locale/locale";
-import {removeEmptyLines} from "./modules/whitespace/lines";
-import {fixNbsp} from "./modules/whitespace/nbsp";
-import {fixSpaces} from "./modules/whitespace/spaces";
-import {fixPeriod} from "./modules/punctuation/period";
-import {fixEllipsis} from "./modules/punctuation/ellipsis";
-import {fixHyphen} from "./modules/punctuation/hyphen";
-import {fixDash} from "./modules/punctuation/dash";
-import {fixDoubleQuotesAndPrimes} from "./modules/punctuation/double-quotes";
-import {fixSingleQuotesPrimesAndApostrophes} from "./modules/punctuation/single-quotes";
-import {fixMultiplicationSign} from "./modules/symbols/multiplication-sign";
-import {fixSectionSign} from "./modules/symbols/section-sign";
-import {fixCopyrights} from "./modules/symbols/copyrights";
-import {fixPlusMinus} from "./modules/symbols/plus-minus";
-import {fixMarks} from "./modules/symbols/marks";
-import {fixExponents} from "./modules/symbols/exponents";
-import {fixNumberSign} from "./modules/symbols/number-sign";
-import {fixAbbreviations} from "./modules/words/abbreviations";
-import {fixCase} from "./modules/words/case";
-import {fixPubId} from "./modules/words/pub-id";
-import {excludeExceptions,
-        placeExceptions} from "./modules/words/exceptions";
-
+import { removeEmptyLines } from "./modules/whitespace/lines";
+import { fixNbsp } from "./modules/whitespace/nbsp";
+import { fixSpaces } from "./modules/whitespace/spaces";
+import { fixPeriod } from "./modules/punctuation/period";
+import { fixEllipsis } from "./modules/punctuation/ellipsis";
+import { fixHyphen } from "./modules/punctuation/hyphen";
+import { fixDash } from "./modules/punctuation/dash";
+import { fixDoubleQuotesAndPrimes } from "./modules/punctuation/double-quotes";
+import { fixSingleQuotesPrimesAndApostrophes } from "./modules/punctuation/single-quotes";
+import { fixMultiplicationSign } from "./modules/symbols/multiplication-sign";
+import { fixSectionSign } from "./modules/symbols/section-sign";
+import { fixCopyrights } from "./modules/symbols/copyrights";
+import { fixPlusMinus } from "./modules/symbols/plus-minus";
+import { fixMarks } from "./modules/symbols/marks";
+import { fixExponents } from "./modules/symbols/exponents";
+import { fixNumberSign } from "./modules/symbols/number-sign";
+import { fixAbbreviations } from "./modules/words/abbreviations";
+import { fixCase } from "./modules/words/case";
+import { fixPubId } from "./modules/words/pub-id";
+import { excludeExceptions, placeExceptions } from "./modules/words/exceptions";
 
 /*
   Correct typos
@@ -36,22 +34,24 @@ import {excludeExceptions,
   @returns {string} corrected output
 */
 export function fixTypos(string, locale, configuration) {
-  locale = (typeof locale === "undefined") ? "en-us" : locale;
+  locale = typeof locale === "undefined" ? "en-us" : locale;
 
   let currentLocale = new Locale(locale);
 
-  configuration = (typeof configuration === "undefined") ? {
-    removeLines : true,
-    removeWhitespacesBeforeMarkdownList : true,
-    keepMarkdownCodeBlocks : false,
-  } : configuration;
+  configuration =
+    typeof configuration === "undefined"
+      ? {
+          removeLines:                         true,
+          removeWhitespacesBeforeMarkdownList: true,
+          keepMarkdownCodeBlocks:              false,
+        }
+      : configuration;
 
   // exclude exceptions from fixing
   const { processedText, exceptions } = excludeExceptions(string, currentLocale);
   string = processedText;
 
-
-  if(configuration.removeLines) {
+  if (configuration.removeLines) {
     string = removeEmptyLines(string);
   }
 
