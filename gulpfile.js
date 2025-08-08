68357de54f621e86f9d16c80c1075ce8f21641e9
Brano Sandala <surfinzap@users.noreply.github.com>
2025-08-08 10:50:12 +0200

    swc minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const browsersync = require("browser-sync").create();
-const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const browserify = require('browserify');
 const babelify = require('babelify');
@@ -9,6 +8,8 @@ const buffer = require('vinyl-buffer');
 const replace = require('gulp-replace');
 const header = require('gulp-header');
 const packageJson = require('./package.json');
+const { transform } = require('@swc/core');
+const through = require('through2');
 
 const currentYear = new Date().getFullYear();
 
@@ -43,6 +44,82 @@ const copyrightBanner = `/*!
  */
 `;
 
+function swcMinify() {
+  return through.obj(async function(file, encoding, callback) {
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    if (file.isStream()) {
+      return callback(new Error('Streaming not supported'));
+    }
+
+    try {
+      const result = await transform(file.contents.toString(), {
+        jsc: {
+          parser: {
+            syntax: 'ecmascript',
+            jsx: false
+          },
+          target: 'es2015',
+          minify: {
+            compress: {
+              arguments: true,
+              arrows: true, 
+              booleans: true,
+              collapse_vars: true,
+              comparisons: true,
+              computed_props: true,
+              conditionals: true,
+              dead_code: true,
+              drop_console: false,
+              drop_debugger: true,
+              evaluate: true,
+              hoist_funs: false,
+              hoist_props: true,
+              hoist_vars: false,
+              if_return: true,
+              join_vars: true,
+              keep_classnames: false,
+              keep_fargs: true,
+              keep_fnames: false,
+              keep_infinity: false,
+              loops: true,
+              negate_iife: true,
+              properties: true,
+              reduce_funcs: false,
+              reduce_vars: false,
+              sequences: true,
+              side_effects: true,
+              switches: true,
+              typeofs: true,
+              unsafe: false,
+              unsafe_arrows: false,
+              unsafe_comps: false,
+              unsafe_Function: false,
+              unsafe_math: false,
+              unsafe_methods: false,
+              unsafe_proto: false,
+              unsafe_regexp: false,
+              unsafe_undefined: false,
+              unused: true
+            },
+            mangle: {
+              toplevel: true
+            }
+          }
+        },
+        minify: true
+      });
+
+      file.contents = Buffer.from(result.code);
+      callback(null, file);
+    } catch (error) {
+      callback(new Error(`SWC minification failed: ${error.message}`));
+    }
+  });
+}
+
 // Update version in typopo.js
 function updateTypopoJsCopyrightBanner() {
 
@@ -71,7 +148,7 @@ function browserBuild() {
 		.bundle()
 		.pipe(source(paths.browser.name))
 		.pipe(buffer())
-		.pipe(uglify())
+		.pipe(swcMinify())
 		.pipe(header(copyrightBanner)) // Add the header
 		.pipe(gulp.dest(paths.browser.dest));
 }
@@ -82,7 +159,7 @@ function npmBuild() {
 		.bundle()
 		.pipe(source(paths.npm.name))
 		.pipe(buffer())
-		.pipe(uglify())
+		.pipe(swcMinify())
 		.pipe(header(copyrightBanner)) // Add the header
 		.pipe(gulp.dest(paths.npm.dest));
 }
